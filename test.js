2a43ed2e5b61d452b0125704c26a932aaa236867
coder.roby <coder.roby@gmail.com>
2021-11-25 22:33:46 +0600

    JS code test on Object and event

--- test.js
+++ test.js
@@ -1,10 +1,30 @@
 let car = {
-    name:"flat",
+    name: "Robi",
     model: "Toyota",
+    wight: "550KG",
     color: "white",
     start: "On",
-    drive: function (){
+    drive: function () {
+        this.position();
         console.log("car is driving");
     },
+    position: function (){
+        console.log("we are in Running position now.");
+    }
 
 }
+
+console.log(car.name);
+console.log(car.wight);
+//console.log(car["start"]);
+car.drive();
+console.log(typeof(car.position()));
+
+/*
+let x = 5;
+let y = new Number(5);
+console.log(typeof(x));
+console.log(typeof(y));
+*/
+
+
