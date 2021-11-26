273894705b95fe160951866f07fb7078b07416aa
coder.roby <coder.roby@gmail.com>
2021-11-26 21:33:51 +0600

    learn some String method

--- string.js
+++ string.js
@@ -1,20 +1,24 @@
-*/
-
+/*
 // let text = "asdanhfsdjfdsafohdoaisi";
 // console.log(text.length);
+*/
 
-/*let text = "We are the so-called "Vikings" from the north.";
+/*
+//=================string escape characters  \" '\ \\ =========================
+let text = "We are the so-called "Vikings" from the north.";
 let text = "We are the so-called \"Vikings\" from the north.";
 console.log(text); */
 
 /*
+//===========================string value , object , type.=======================
 let x = "Jhon";
 let Y = new String("Jhon");
 console.log(x==Y);
 console.log(x===Y);
-
 */
 
+/*
+//======================================string methods===========================
 let car = "abcdeefghij";
 console.log(car.slice(4,11));
 console.log(car.slice(-13,-8));
@@ -24,3 +28,37 @@ console.log(car.substring(7,10));
 //console.log(car.substring(-15,-10)); xxxx
 console.log(car.substr(3, 5));
 console.log(car.substr(3));
+*/
+
+/*
+//============================= replace method================
+let love = " I love Robi";
+let b = love.replace("Robi", "Code");
+console.log(love);
+console.log(b);
+let c = love.slice(0,8) + "Slice method";
+console.log(c);
+*/
+
+/* 
+//============================trim method===========================
+let x = "     Hello World";
+console.log(x);
+console.log(x.trim());
+*/
+
+/*
+var str = "HELLO WORLD";
+console.log(typeof str.charAt(500));   // string
+console.log(typeof str[500]);          // Undefined  --- value + data type
+*/
+
+
+//============================split method===========================
+var str = "HELLO WORLD";
+console.log(str.split(""));
+console.log(str.split(" "));
+var str = "HELLO,WORLD";
+console.log(str.split(","));
+
+ 
--- test.js
+++ test.js
@@ -28,30 +28,5 @@ let x = 5;
 let y = new Number(5);
 console.log(typeof(x));
 console.log(typeof(y));
-*/
-
-// let text = "asdanhfsdjfdsafohdoaisi";
-// console.log(text.length);
-
-/*let text = "We are the so-called "Vikings" from the north.";
-let text = "We are the so-called \"Vikings\" from the north.";
-console.log(text); */
-
-/*
-let x = "Jhon";
-let Y = new String("Jhon");
-console.log(x==Y);
-console.log(x===Y);
 
 */
-
-let car = "abcdeefghij";
-console.log(car.slice(4,11));
-console.log(car.slice(-13,-8));
-console.log(car.slice(5));
-console.log(car.substring(7));
-console.log(car.substring(7,10));
-//console.log(car.substring(-15,-10)); xxxx
-console.log(car.substr(3, 5));
-console.log(car.substr(3));
-
