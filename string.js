b19b1697f1010583eaa4ade779902080722034dd
coder.roby <coder.roby@gmail.com>
2021-11-26 18:16:22 +0600

    new string.js file attached

--- string.js
+++ string.js
@@ -1,33 +1,3 @@
-/*
-let car = {
-    name: "Robi",
-    model: "Toyota",
-    wight: "550KG",
-    color: "white",
-    start: "On",
-    drive: function () {
-        this.position();
-        console.log("car is driving");
-    },
-    position: function (){
-        console.log("we are in Running position now.");
-    }
-
-}
-
-console.log(car.name);
-console.log(car.wight);
-//console.log(car["start"]);
-car.drive();
-console.log(typeof(car.position()));
-
-*/
-
-/*
-let x = 5;
-let y = new Number(5);
-console.log(typeof(x));
-console.log(typeof(y));
 */
 
 // let text = "asdanhfsdjfdsafohdoaisi";
@@ -54,4 +24,3 @@ console.log(car.substring(7,10));
 //console.log(car.substring(-15,-10)); xxxx
 console.log(car.substr(3, 5));
 console.log(car.substr(3));
-
