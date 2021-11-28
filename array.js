74c4dd8ec43f3f99ff03d4ab569a776927a6a18a
coder.roby <coder.roby@gmail.com>
2021-11-28 06:25:46 +0600

    learn about array

--- array.js
+++ array.js
@@ -5,7 +5,7 @@ console.log(cars.length);        //array elements number
 console.log(cars[0]);          // first element
 console.log(cars[cars.length -1]); // last element
  */
-
+/* 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let fLen = fruits.length;
 let list = "<ul>";
@@ -22,3 +22,28 @@ list += "</ul";
 document.getElementById("list").innerHTML = list;
 
 
+ */
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.forEach(namelist);
+
+function namelist(value){
+    console.log(value);
+}
+
+fruits.push("lemon");
+fruits[fruits.length] = "jackfruits", "asa";
+//fruits.insert(2,'asddas');
+fruits[7] = "Roby";
+console.log(fruits);
+console.log(fruits[6]);  // Undefiend
+
+
+
+
+
+/* function printlist(Banan){
+    console.log(Banan);
+}
+printlist("liza"); */
