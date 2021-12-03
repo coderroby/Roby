3816e74b102841f1b747c8a94aa28289267cf388
coder.roby <coder.roby@gmail.com>
2021-12-04 01:29:45 +0600

    learn some conditions

--- test.js
+++ test.js
@@ -30,3 +30,48 @@ console.log(typeof(x));
 console.log(typeof(y));
 
 */
+
+
+// Boolean Data
+
+console.log(typeof Boolean(10>9));
+console.log(Boolean(10>9));
+console.log(8<6);
+
+let a = 5;
+let b = "5";
+let c = 2;
+console.log(a==b);    // check values
+console.log(a===b);   // check values and data type
+
+if(a==5 && b === 5 /* || c === 2 */ ){
+    console.log("yes condition applied");
+}
+else console.log("No condition applied");
+
+// =======ternary operator===== if else er onno vabe use kora jai
+//variablename = (condition) ? value1:value2 
+
+console.log(a== 5 && b===2 ? "confition true" : "Condition false");
+console.log(a >= 5 ? a >2 && a !=5 ? "your are just a baby": "you are more then 2!" : " here a = 5");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
