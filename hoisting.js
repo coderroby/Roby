d211c2053b564a61613697678178f506f1ccd31e
coder.roby <coder.roby@gmail.com>
2021-12-08 05:46:18 +0600

    Still facting sissue of var hoisting

--- hoisting.js
+++ hoisting.js
@@ -1,2 +1,32 @@
+/* let a;
 console.log(a);
-var a = 5;
+a = 5;
+console.log(a); */
+
+var LANGUAGE = "java";
+var language = "JavaScript";
+console.log(!language);
+
+
+function getLanguage(){
+    console.log(!language);
+    if(!language){
+        console.log(!language);
+        var language = LANGUAGE;
+        console.log(language);
+
+    }
+    return language;
+}
+
+getLanguage(); 
+
+
+function myfunction(){
+    lan = "javasx";
+    var lan;
+    console.log(lan);
+}
+
+myfunction();
+console.log(lan);
