9254e438408fde9f5ca312ddb66ec499f657675d
coder.roby <coder.roby@gmail.com>
2021-12-09 01:13:48 +0600

    strict mode

--- hoisting.js
+++ hoisting.js
@@ -15,7 +15,7 @@ function getLanguage(){
     if(!language){
         
         console.log(!language);
-        let language = LANGUAGE;
+        language = LANGUAGE;
         console.log(language);
 
     }
